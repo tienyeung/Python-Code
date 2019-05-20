53390b1c845c8c962b9a6179050741ab6cda7905
tienyeung <yeungtien@gmail.com>
2019-05-20 19:24:23 +0800

    pdf and word

---
+++ document_proceed/word/word.py
@@ -0,0 +1,17 @@
+import docx
+
+# read the docx
+# def getText(filename):
+#     doc=docx.Document(filename)
+#     fulltext=[]
+#     for para in doc.paragraphs:
+#         fulltext.append(para.text)
+#     return '\n'.join(fulltext)
+
+# write to docx
+# doc=docx.Document()
+# doc.add_paragraph('hello,world')
+# add title ,0 means 1st title
+# doc.add_heading('str',0)
+# 换行：add_break 换页:add_break(docx.text.WD_BREAK.PAGE)
+# doc.save('hello_world.docx')
