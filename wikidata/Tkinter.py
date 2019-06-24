50a0787ac24a336c0cc83b6699cfbfd00a9b7717
tienyeung <yeungtien@gmail.com>
2019-06-24 20:13:30 +0800

    scrapy

--- wikidata/Tkinter.py
+++ wikidata/Tkinter.py
@@ -63,29 +63,7 @@ class Application(Frame):
         app.bttn5.grid(row=1, column=7, ipadx=1, ipady=1,
                        sticky=W, padx=10, pady=10)
 
-        # app.label3 = Label(app, text="Optimize(INDEX) :")
-        # app.label3.grid(row=2, column=0, columnspan=10,
-        #                 sticky=W, padx=5, pady=5)
-
-        # app.bttn6 = Button(app, text="name->entity", command=app.cmd6)
-        # app.bttn6.grid(row=2, column=2, ipadx=3, ipady=1,
-        #                sticky=W, padx=10, pady=10)
-
-        # app.bttn7 = Button(app, text="id->pre-category", command=app.cmd7)
-        # app.bttn7.grid(row=2, column=3, ipadx=3,
-        #                ipady=1, sticky=W, padx=5, pady=5)
-
-        # app.bttn8 = Button(app, text="id->all-entity", command=app.cmd8)
-        # app.bttn8.grid(row=2, column=4, ipadx=3, ipady=1,
-        #                sticky=W, padx=10, pady=10)
-
-        # app.bttn9 = Button(app, text="id->all-property", command=app.cmd9)
-        # app.bttn9.grid(row=2, column=5, ipadx=3, ipady=1,
-        #                sticky=W, padx=10, pady=10)
-
-        # app.bttn0 = Button(app, text="Q&A", command=app.cmd10)
-        # app.bttn0.grid(row=2, column=7, ipadx=1, ipady=1,
-        #                sticky=W, padx=10, pady=10)
+       
 
         app.label4 = Label(app, text="Result:")
         app.label4.grid(row=3, column=0, columnspan=8,
@@ -245,117 +223,6 @@ class Application(Frame):
         app.scr1.insert(END, app.time)
 
 
-# index
-    # def cmd6(app):
-    #     # sql_index= 'alter table entity add index label_index (qlabel(88))'
-    #     # cur.execute(sql_index)
-    #     start = time.clock()
-    #     input1 = app.var1.get()
-    #     sql = 'select * from entity where qlabel like "%%%%%s%%%%%%"' % (
-    #         input1)
-    #     cur.execute(sql)
-    #     rows = cur.fetchall()
-    #     # print (rows)
-    #     app.scr.delete(0.0, END)
-    #     app.scr1.delete(0.0, END)
-    #     for row in rows:
-    #         id = row[0]
-    #         type = row[1]
-    #         label = row[2]
-    #         aliase = row[3]
-    #         description = row[4]
-    #         result = 'id:'+id.ljust(12)+'     '+'type:'+type.ljust(8)+'     '+'label:'+label.ljust(
-    #             8)+'     '+'aliase:'+aliase.ljust(8)+'     '+'description:'+description.ljust(8)+'\n'
-    #         app.quote = result
-    #         app.scr.insert(END, app.quote)
-    #     end = time.clock()
-    #     app.time = end-start
-    #     app.scr1.insert(END, app.time)
-
-    # def cmd7(app):
-    #     sql_index_1= 'create index qid_claim_index on claim(qid)'
-    #     cur.execute(sql_index_1)
-    #     sql_index_2= 'create index pid_property_index on property(pid)'
-    #     cur.execute(sql_index_2)
-    #     start = time.clock()
-    #     input1 = app.var1.get()
-    #     sql = 'select pid,plabel from property where plabel="subclass of" or "instance of" in (select plabel from property where pid in (select pid from claim where qid ="%s")) ' % (
-    #         input1)
-    #     cur.execute(sql)
-    #     rows = cur.fetchall()
-    #     app.scr.delete(0.0, END)
-    #     app.scr1.delete(0.0, END)
-    #     print(rows)
-    #     for row in rows:
-    #         # result = '%s' + 'instance of' + 'property:'+row[0]+'\n' % input1
-    #         result = row[1]+' '+'property:'+row[0]+'\n'
-    #         app.quote = result
-    #         app.scr.insert(END, app.quote)
-    #     end = time.clock()
-    #     app.time = end-start
-    #     app.scr1.insert(END, app.time)
-
-    # def cmd8(app):
-    #     sql_index_1= 'create index type_claim_index on claim(datavalue_type)'
-    #     cur.execute(sql_index_1)
-    #     start = time.clock()
-    #     input1 = app.var1.get()
-    #     sql = 'select datavalue_value from claim where qid ="%s" and datavalue_type="string"' % (
-    #         input1)
-    #     cur.execute(sql)
-    #     rows = cur.fetchall()
-    #     #print (rows)
-    #     app.scr.delete(0.0, END)
-    #     app.scr1.delete(0.0, END)
-    #     for row in rows:
-    #         # print(row[0])
-    #         result = 'entityid:'+row[0]+'\n'
-    #         app.quote = result
-    #         app.scr.insert(END, app.quote)
-    #     end = time.clock()
-    #     #print("cost time:")
-    #     app.time = end-start
-    #     app.scr1.insert(END, app.time)
-
-    # def cmd9(app):
-    #     start = time.clock()
-    #     input1 = app.var1.get()
-    #     sql = 'select pid from claim where qid="%s" ' % (input1)
-    #     cur.execute(sql)
-    #     rows = cur.fetchall()
-    #     #print (rows)
-    #     app.scr.delete(0.0, END)
-    #     app.scr1.delete(0.0, END)
-    #     for row in rows:
-    #         # print(row[0])
-    #         result = 'property:' + \
-    #             row[0].ljust(20)+'\n'
-    #         app.quote = result
-    #         app.scr.insert(END, app.quote)
-    #     end = time.clock()
-    # #print("cost time:")
-    #     app.time = end-start
-    #     app.scr1.insert(END, app.time)
-
-    # def cmd10(app):
-    #     start = time.clock()
-    #     input1 = app.var1.get()
-    #     sql = 'select datavalue_value from claim where pid in (select pid from property where plabel like "%%%%%s%%%%%%") and qid in (select qid from entity where qlabel like "%%%%%s%%%%%%")' % (
-    #         input1)
-    #     cur.execute(sql)
-    #     rows = cur.fetchall()
-    # #print (rows)
-    #     app.scr.delete(0.0, END)
-    #     app.scr1.delete(0.0, END)
-    #     for row in rows:
-    #         # print(row[0])
-    #         result = 'value:'+row[0]+'\n'
-    #         app.quote = result
-    #         app.scr.insert(END, app.quote)
-    #     end = time.clock()
-    # #print("cost time:")
-    #     app.time = end-start
-    #     app.scr1.insert(END, app.time)
 
 
 if __name__ == '__main__':
